38ef014ae58e54f856e842a25ec1c01c096c7cbf
Adam Ryman <adamryman@gmail.com>
2016-10-01 21:18:07 -0700

    Add dev tool and docker install scripts

--- other_install/dev.sh
+++ other_install/dev.sh
@@ -5,7 +5,7 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 
-sudo apt install git i3 xfonts-terminus rxvt-unicode-256color keynav xclip python-dev python-pip python3-pip python3-dev neovim firefox
+sudo apt install git i3 xfonts-terminus rxvt-unicode-256color keynav xclip python-dev python-pip python3-pip python3-dev neovim firefox tmux htop redshift
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
---
+++ other_install/docker_ubuntu.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+sudo apt install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+# This needs to changed to the version of ubuntu/debian you are using
+echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' | sudo tee -a /etc/apt/sources.list.d/docker.list
+sudo apt update
+sudo apt-cache policy docker-engine
+
+# Only ubuntu
+sudo apt update
+sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+sudo apt update
+sudo apt install docker-engine
+sudo service docker start
+
+# group in ubuntu
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# docker-compose
+pip install docker-compose
