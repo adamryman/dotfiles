dc48b52bae34b2bebc42836ac71f0eeabb2359bc
Adam Ryman <adamr@tune.com>
2016-10-03 12:07:15 -0700

    Fix install ftplugin folder

--- install.sh
+++ install.sh
@@ -22,8 +22,9 @@ function basedotfilesinstall {
 	files[20]=".vimrc"
 	files[21]=".vim/colors"
 	files[22]=".vim/ftplugin"
+
 	# nvim
-	files[22]=".config/nvim"
+	files[23]=".config/nvim"
 
 	files[30]=".vimperatorrc"
 
