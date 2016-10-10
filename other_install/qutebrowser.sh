5bd78fe313e885bd4ee03ab078fa1524f252b2c9
Adam Ryman <adamryman@gmail.com>
2016-10-10 13:36:53 -0700

    Fix u to y

--- other_install/qutebrowser.sh
+++ other_install/qutebrowser.sh
@@ -4,6 +4,6 @@ mkdir -p $HOME/external/qutebrowser
 cd $HOME/external/qutebrowser
 wget https://github.com/The-Compiler/qutebrowser/releases/download/v0.8.2/qutebrowser_0.8.2-1_all.deb
 wget https://qutebrowser.org/python3-pypeg2_2.15.2-1_all.deb
-sudo apt install -u python3-lxml python-tox python3-pyqt5 python3-pyqt5.qtwebkit python3-sip python3-jinja2 python3-pygments python3-yaml
+sudo apt install -y python3-lxml python-tox python3-pyqt5 python3-pyqt5.qtwebkit python3-sip python3-jinja2 python3-pygments python3-yaml
 sudo dpkg -i python3-pypeg2_2.15.2-1_all.deb 
 sudo dpkg -i qutebrowser_0.8.2-1_all.deb 
