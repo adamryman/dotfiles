e185a7a6d69966dc15e25e4725f7fd2c961b4174
Adam Ryman <adamryman@gmail.com>
2016-10-31 14:42:57 -0700

    Improved ag replace

---
+++ scripts/ag-replace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage: $(basename $0) \"THIS\" \"THAT\"";
+	echo "Outputs files modified"
+	exit 1;
+}
+
+
+replace() {
+	excaped1=$(echo "$1" | sed -e 's/[\/&]/\\&/g');
+	excaped2=$(echo "$2" | sed -e 's/[\/&]/\\&/g');
+
+	# list files with this literal string, case sensitive
+	files=$(ag "$1" -Qls)
+	echo "$files" | while read line; do
+		echo "$line"
+		sed -i "s/$excaped1/$excaped2/g" "$(pwd)"/"$line";
+	done;
+}
+
+
+if [ "$#" -ne  2 ]; then
+	 usage; 
+	 exit 1;  
+else
+	 replace "$@";
+fi;
