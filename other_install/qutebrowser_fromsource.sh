3860299a8f90cb1aa11f877a5061c168d938de51
Adam Ryman <adamryman@gmail.com>
2016-12-27 10:54:30 -0800

    Start the   qutebrowser from source script

---
+++ other_install/qutebrowser_fromsource.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+echo "enter sudo your password"
+sudo echo "thanks"
+
+mkdir -p $HOME/external/qutebrowser
+sudo apt update
+sudo apt install -y python3-pyqt5 python3-pyqt5.qtwebkit python-tox python3-sip
+# :help
+sudo apt-get install asciidoc source-highlight
+cd $HOME/external/qutebrowser
+git clone https://github.com/The-Compiler/qutebrowser.git .
+tox -e mkvenv
+echo "#!/bin/bash" > $HOME/bin/qutebrowser
+echo "$(pwd)/.venv/bin/python3 -m qutebrowser \$@" >> $HOME/bin/qutebrowser
+chmod 755 $HOME/bin/qutebrowser
+echo "done"
