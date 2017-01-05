f772e0889fcedf172b288f0f2e4243e6667a08f7
Adam Ryman <adamryman@gmail.com>
2017-01-04 16:36:55 -0800

    Update neovim install to install 1.6 for now

--- other_install/neovim_fromsource.sh
+++ other_install/neovim_fromsource.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
-sudo apt-get -y install libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+sudo apt-get -y install libtool libtool-bin autoconf automake cmake g++
+pkg-config unzip libunibilium-dev
 mkdir -p $HOME/external/neovim
 cd $HOME/external/neovim
 git clone https://github.com/neovim/neovim .
+# For whatever reason vim-go is not working on 0.2.0 or tip. This is the one
+# @zaquestion uses
+git checkout v0.1.6
 mkdir build
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/external/neovim/build"
 make install
