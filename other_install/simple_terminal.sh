62d4d35e3150346fd5063475e50adaea7031cfab
Adam Ryman <adamryman@gmail.com>
2017-09-08 12:53:02 -0700

    Add echos

--- other_install/simple_terminal.sh
+++ other_install/simple_terminal.sh
@@ -1,28 +1,31 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
+set -e
+[[ -z $DEBUG ]] || set -x
 echo "Please enter your password."
 sudo echo "thanks"
 
-"Installing dependencies"
+echo "Installing dependencies"
 sudo apt install -y x11proto-core-dev libx11-dev libxft-dev
 
-"Cloning and building"
+echo "Cloning and building"
 mkdir -p $HOME/external/st
 cd $HOME/external/st
 git clone http://git.suckless.org/st .
 make
 
 # config
-"Linking config.h and st.1 (for config and docs)"
+echo "Linking config.h and st.1 (for config and docs)"
 rm config.h
 rm st.1
 mkdir -p $HOME/.config/st
 ln -s $HOME/.config/st/config.h $HOME/external/st/config.h
 ln -s $HOME/.config/st/st.1 $HOME/external/st/st.1
 
-"Rebuilding"
+echo "Rebuilding"
 make
 
-"Adding st to $HOME/bin"
+echo "Adding st to $HOME/bin"
 mkdir -p $HOME/bin
 ln -s $HOME/external/st/st $HOME/bin/st
 
