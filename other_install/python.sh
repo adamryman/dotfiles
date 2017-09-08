ba48e92c533b14a043741e1c852114a432436290
Adam Ryman <adamryman@gmail.com>
2017-09-08 16:15:12 -0700

    Add python dev config

---
+++ other_install/python.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+set -e
+[[ -z $DEBUG ]] || set -x
+sudo apt install -y virtualenv
+cd $HOME/bin
+virtualenv -p $(which python3) venv3
+
+#venv3
+# How do I do this in my venv?
+
+# Has a sql formatter sqlforma
+#pip install --upgrade sqlparse
+# format sql in the clipboard
+#alias fmtsql='sqlformat --reindent --keywords upper  <(xclip -o -sel clip) | sponge | xclip -i -sel clip'
