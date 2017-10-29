830305bd265fe435359d3d69e6b601d05a0d4bbc
Adam Ryman <adamr@tune.com>
2017-10-29 12:17:12 -0700

    Move vim vundle and youcomplete me to dev install.

--- other_install/dev.sh
+++ other_install/dev.sh
@@ -51,3 +51,9 @@ sudo cp /tmp/image-go-fonts/font/gofont/ttfs/Go-Mono* /usr/share/fonts/truetype/
 curl `curl -s -L https://golang.org/dl | grep 'download downloadBox.\+linux-amd64' | cut -d'"' -f 4` > /tmp/golang.tar.gz
 test -r /usr/local/go && sudo rm -rf /usr/local/go
 sudo tar -C /usr/local/ -xzf /tmp/golang.tar.gz
+
+# Config Applications
+echo "===== nvim Environment ====="
+test ! -d ~/.vim/bundle/Vundle.vim && git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim && \
+nvim +PluginInstall +GoInstallBinaries +qall && \
+(cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer --gocode-completer)
--- other_install/vundle.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-mkdir -p $HOME/.vim/bundle/ && \
-	cd $HOME/.vim/bundle/  && \
-git clone --depth 1 https://github.com/gmarik/Vundle.vim.git 
