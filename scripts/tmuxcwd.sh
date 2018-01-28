46e72c8b3df2d410237db1d10af40cdd7d1b7008
Adam Ryman <adamryman@gmail.com>
2018-01-28 14:25:01 -0800

    Exec removes the bash process that launched tmux

--- scripts/tmuxcwd.sh
+++ scripts/tmuxcwd.sh
@@ -7,6 +7,15 @@
 # focused window you must send input to the window to guarantee that it was the
 # last active tmux client. In i3 this can be done with 
 # `bindsym --release key exec "xdotool key ctrl+shift+at"`
+IS_TMUX_WINDOW="";
+
+#if xprop -id $(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}') | grep WM_NAME | grep tmux 2>/dev/null 1>/dev/null; then
+#if xdotool getwindowname $(xdotool getactivewindow) | grep tmux 2>/dev/null 1>/dev/null; then
+	#sleep 1
+	#xdotool key ctrl+b Escape && sleep 1;
+#fi
+
+
 CURRENT_PANE_PATH="$(tmux list-clients -F '#{client_activity} #{pane_current_path}' | sort | awk '{ print $2 }' | tail -1)";
 
-tmux new-session -c "$(if [[ -n $CURRENT_PANE_PATH ]]; then echo $CURRENT_PANE_PATH; else echo $HOME; fi)";
+exec tmux new-session -c "$(if [[ -n $CURRENT_PANE_PATH ]]; then echo $CURRENT_PANE_PATH; else echo $HOME; fi)";
